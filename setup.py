4c22def2bd45bacf40a63337861ec4934b736fad
Stuart McAlpine <stuart.mcalpine@helsinki.fi>
2022-02-10 01:10:11 +0200

    Added dist install

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='read_swift',
+      version='0.1',
+      description='MPI script for reading SWIFT simulation outputs',
+      author='Stuart McAlpine',
+      author_email='stuart.mcalpine@helsinki.fi',
+      py_modules=['read_swift']
+     )
